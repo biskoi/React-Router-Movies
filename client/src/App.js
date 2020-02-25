bd15e005a0b7c67f96c5b241bb98a9791d39a61c
biskoi <biskoi@daydream.cafe>
2020-02-25 14:05:18 -0700

    Project MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-
+import {Route, Switch} from 'react-router-dom';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 import SavedList from './Movies/SavedList';
 
 const App = () => {
@@ -12,7 +14,12 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+
+      <Switch>
+        <Route path = '/movies/:movieID' component = {Movie}/>
+        <Route path = '/' component = {MovieList}/>
+      </Switch>
+
     </div>
   );
 };
